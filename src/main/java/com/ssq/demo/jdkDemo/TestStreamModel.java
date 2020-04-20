65c147d3c1966007fab6917bdfb5c2f0fa000963
sushiqi <sushiqi@jd-auto.com.cn>
2020-04-20 11:12:37 +0800

    stream流的列表demo

---
+++ src/main/java/com/ssq/demo/jdkDemo/TestStreamModel.java
@@ -0,0 +1,17 @@
+package com.ssq.demo.jdkDemo;
+
+import lombok.Data;
+
+@Data
+public class TestStreamModel {
+    private int id;
+
+    private String name;
+
+    private int grade;
+
+    private int classes;
+
+    private double score;
+
+}
