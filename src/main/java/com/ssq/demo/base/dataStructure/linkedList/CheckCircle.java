c4065a1ec2bce5184fcca89a8a4a9836123a75bb
sushiqi <sushiqi@meishubao.com>
2021-07-25 18:53:43 +0800

    算法 --> 单链表之判断链表内是否有环(带总结)

--- src/main/java/com/ssq/demo/base/dataStructure/linkedList/CheckCircle.java
+++ src/main/java/com/ssq/demo/base/dataStructure/linkedList/CheckCircle.java
@@ -1,9 +1,12 @@
 package com.ssq.demo.base.dataStructure.linkedList;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.ObjectUtils;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * @Author: 苏诗淇
@@ -19,116 +22,170 @@ import java.util.HashSet;
  *          2-2.环的长度
  *          2-3.判断两个单向链表是否相交，如果相交，求出交点
  */
-public class CheckCircle {
+public class CheckCircle<T> {
+
     /**
-     * 定义指针
-     *
+     * 创建一个指针类
      * @param <T>
      */
     @Data
-    static class Node<T> {
+    @NoArgsConstructor
+    @AllArgsConstructor
+    class Node<T> {
         T data;
-        Node next;
+        Node<T> next;
 
         public Node(T data) {
             this.data = data;
         }
-
-        public Node(T data, Node next) {
-            this.data = data;
-            this.next = next;
-        }
-
-        public T getNextNodeData() {
-            return next != null ? (T) next.data : null;
-        }
-
     }
 
     /**
-     * 1.快慢指针法
-     *
-     * @param head
+     * 头结点
+     */
+    Node<T> head;
+
+    /**
+     * 该链表长度
+     */
+    int size;
+
+    /**
+     * 初始化链
+     */
+    public void init() {
+        head = new Node<>(null, null);
+        head.next = null;
+        size = 0;
+    }
+
+    /**
+     * 根据下标/元素值查找元素
+     * @param data
+     * @param i
      * @return
      */
-    public static boolean hasLoopFS(Node head) {
-        if (null == head) {
-            return false;
+    public Node<T> queryNode(T data, int i) {
+        Node<T> newNode = head;
+        // 按元素查找
+        if (!ObjectUtils.isEmpty(data)){
+            //否则循环链表,查找是否存在值相同的元素
+            while (null != newNode.next) {
+                if (data.equals(newNode.data)) {
+                    return newNode;
+                }
+                newNode = newNode.next;
+            }
+            return null;
         }
 
-        Node slow = head;
-        Node fast = head.next;
-        // 快指针未能遍历完所有节点
-        while (null != fast && null != fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) {
+        // 按下标查找,判断下标是否大于0,小于总链表长度
+        if (0 < i && i <= size) {
+          // 寻找链表内符合的下标元素
+            for (int q = 1; q < i; q++) {
+                newNode = newNode.next;
+            }
+            return newNode;
+        }
+        return null;
+    }
+
+    /**
+     * 添加元素
+     * @param data
+     */
+    public void add(T data) {
+        // 创建一个新结点
+        Node<T> newNode = new Node(data);
+        // 判断头结点是否为空,若为空,直接给头结点赋值
+        if (0 == size) {
+            head = newNode;
+        } else {
+            // 否则遍历整个链表,将元素插入至链表尾部
+            Node temp = head;
+            while (null != temp.next) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        size++;
+    }
+
+    /**
+     * 链表是否存在闭环之快慢指针法(最优解,时间复杂度为O(N), 空间复杂度为1)
+     *  思路:
+     *      1.创建快/慢两个指针
+     *      2.慢指针指头结点,快指针指向下一个节点
+     *      3.判断快指针和快指针的下一个是否为空,不为空则指针继续往下指
+     *      4.慢指针指向下一个,快指针指向后两个(相当于快指针的速度是慢指针的2倍,所以若有闭环,快慢指针早晚会重合相遇)
+     * @return
+     */
+    public Boolean isCircle(Node<T> linkedList) {
+        // 判断该指针是否为空,为空则不存在闭环
+        if (null == linkedList) {
+            return false;
+        }
+        // 创建两个指针
+        Node<T> slowNode = linkedList;
+        Node<T> quickNode = linkedList.next;
+        // 两个指针同时出发,若循环后两指针元素值相同,则说明存在闭环
+        while (null != quickNode && null != quickNode.next) {
+            slowNode = slowNode.next;
+            quickNode = quickNode.next.next;
+            if (slowNode == quickNode) {
                 return true;
             }
         }
         return false;
     }
 
-    // 设一个缓存点,保存足迹信息
-    private static HashMap<Object, Integer> nodeMap = new HashMap<>();
-
+    private HashMap<Object, Integer> code = new HashMap<>();
     /**
-     * 2.足迹法
-     * @param node
-     * @param index
+     * 链表是否存在闭环之足迹法
+     *  思路:
+     *      1.判断该链表是否为空或只有一个节点
+     *      2.判断哈希表里是否存在相同的链表节点,若存在,则证明有环
+     *      3.否则将该节点及索引值放入哈希表内,递归循环后续节点
+     *  弊端:
+     *      若该链表内存在相同元素的节点,则会判断有误
+     *  复杂度:
+     *      时间复杂度: O(N)
+     *      空间复杂度: O(N)
+     * @param linkedList 链表
+     * @param index 索引
      * @return
      */
-    public static boolean hasLoopHash(Node node, Integer index) {
-        if (null == node || null == node.next) {
+    public Boolean isCircleFootprint(Node<T> linkedList, Integer index) {
+        // 判断该链表是否为空或者只存在一个节点
+        if (null == linkedList || null == linkedList.next) {
             return false;
         }
-        if (nodeMap.containsKey(node.data)) {
+        // 判断哈希表里是否存在相同的链表节点,若存在,证明有环,否则无
+        if (code.containsKey(linkedList.data)) {
             return true;
         } else {
-            nodeMap.put(node.data, index);
-            return hasLoopHash(node.next, ++index);
-        }
-
-    }
-
-    public static Object queryLookEntry(Node node, Integer index) {
-        // 判断该结点是否为空
-        if (null == node || null == node.next) {
-            return null;
-        }
-        if (nodeMap.containsKey(node)) {
-            return node.data;
-        } else {
-            nodeMap.put(node.data, index);
-            return queryLookEntry(node.next, ++index);
+            code.put(linkedList.data, index);
+            return isCircleFootprint(linkedList.next, ++index);
         }
     }
+
+
 
     public static void main(String[] args) {
-        Node link1 = new Node(1);
-        Node link2 = new Node(2);
-        Node link3 = new Node(3);
-        Node link4 = new Node(4);
-        Node link5 = new Node(5);
-        Node link6 = new Node(6);
-        Node link7 = new Node(7);
-        Node link8 = new Node(8);
-        link1.next = link2;
-        link2.next = link3;
-        link3.next = link4;
-        link4.next = link5;
-        link5.next = link6;
-        link6.next = link7;
-        link7.next = link8;
-        link8.next = link4;
-//        boolean hasLoopFS = hasLoopFS(link1);
-//        if (hasLoopFS == true) {
-//            System.out.println("有环");
-//        }
-        boolean b = hasLoopHash(link1, 1);
-        if (b == true) {
-            System.out.println("有环");
+        CheckCircle checkCircle = new CheckCircle();
+
+        String str = "热,爱,生,活,热,爱,你";
+        String[] split = str.split(",");
+        for (String s : split) {
+            checkCircle.add(s);
         }
+        // 给链表添加环
+        CheckCircle.Node newHead = checkCircle.head;
+        checkCircle.queryNode(null, 7).next = checkCircle.queryNode(null, 5);
+        // 执行环检测
+        Boolean isCircle = checkCircle.isCircle(newHead);
+//        Boolean isCircle = checkCircle.isCircleFootprint(node1, 1);
+        System.out.println(isCircle);
 
     }
 
