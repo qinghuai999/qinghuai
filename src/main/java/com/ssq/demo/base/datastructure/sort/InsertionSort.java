8e60b49588bbb7210ec7c2daf1de7d0609d3bbe5
sushiqi <sushiqi@meishubao.com>
2021-08-05 19:45:03 +0800

    数据结构 --> 选择排序 数据结果基础知识文档 - ++i与i++                    选择排序原理                    冒泡,插入,选择排序对比

--- src/main/java/com/ssq/demo/base/datastructure/sort/InsertionSort.java
+++ src/main/java/com/ssq/demo/base/datastructure/sort/InsertionSort.java
@@ -7,16 +7,46 @@ import org.springframework.util.ObjectUtils;
  * @Author: 苏诗淇
  * @CreateDate: 2021-08-04 20:16:07
  * @Description: 插入排序
- * 思路:
- * 1.创建两个数组,一个存放原始元素,一个空数组,用来存放有序元素
- * 2.将原始元素放入有序数组中,然后每次移动元素时进行比较,找到合适的位置在插入.
+ *   思路:
+ *      1.根据下标遍历该数组
+ *      2.创建一个临时变量存放该下标的元素
+ *      3.嵌套循环从当前元素的前一个开始从后往前遍历
+ *      4.若两个元素比较大小后位置错误,则交换位置,直到找到合适的位置,插入
+ *
+ *   小知识:
+ *      1.for循环的++i和i++作用是一样的,因为都是在执行完循环体内的代码之后才会执行++i/i++
+ *      2.执行时间有差距,++i需要一个临时变量存储返回自增前的值,而++i是处理完之后在自增,无需占用内存,所以性能相对好
  */
 public class InsertionSort {
     public void insertionSort(int[] data) {
-
-        for (int i = 0; i < data.length; i++) {
+        // 遍历需要排序的数组
+        for (int i = 0; i < data.length; ++i) {
+            // 得到要排序的元素
             int temp = data[i];
-            // 对有序数组里的元素进行比较
+            // 从后往前遍历该数组,比较元素的大小
+            int j = i - 1;
+            for (; j >= 0; --j) {
+                // 若前一个元素比后一个大,则元素向后移动,否则跳出循环
+                if (data[j] > temp) {
+                    data[j+1] = data[j];
+                } else {
+                    break;
+                }
+            }
+            // 将要排序的元素插入当前下标的位置
+            data[j+1] = temp;
+        }
+        for (int s : data) {
+            System.out.println(s);
+        }
+    }
+
+    public void insertTest(int[] data) {
+        if (data.length == 0) {
+            return;
+        }
+        for (int i = 0; i < data.length; ++i) {
+            int temp = data[i];
             int j = i - 1;
             for (; j >= 0; --j) {
                 if (data[j] > temp) {
@@ -26,9 +56,6 @@ public class InsertionSort {
                 }
             }
             data[j+1] = temp;
-        }
-        for (int s : data) {
-            System.out.println(s);
         }
     }
 
---
+++ src/main/java/com/ssq/demo/base/datastructure/sort/SelectionSort.java
@@ -0,0 +1,41 @@
+package com.ssq.demo.base.datastructure.sort;
+/**
+ * @Author: 苏诗淇
+ * @CreateDate: 2021-08-05 17:53:18
+ * @Description: 选择排序
+ *                  思路: 在原始数组中找出最小的元素,放入数组头部
+ *                      1. 遍历数组,存储下标
+ *                      2. 嵌套遍历数组,每次与第一次存储的下标元素进行比较,若位置错误,存储当前下标
+ *                      3. 继续遍历,知道遍历完整个数组,得到的下标就是最小元素的下标数
+ *                      4. 然后在外层交换两个下标元素的位置
+ *
+ */
+public class SelectionSort {
+    public void selectionSort(int[] data) {
+        if (data.length == 0) {
+            return;
+        }
+        // 1.找到原始数组内最小的元素
+        for (int i = 0; i < data.length; ++i) {
+            int k = i;
+            int j = data.length-1;
+            for (; j >= i; --j) {
+                if (data[j] < data[k]) {
+                    k = j;
+                }
+            }
+            int temp = data[i];
+            data[i] = data[k];
+            data[k] = temp;
+        }
+        for (int a : data) {
+            System.out.println(a);
+        }
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+        int[] a = {9,1,7,6,3};
+        selectionSort.selectionSort(a);
+    }
+}
