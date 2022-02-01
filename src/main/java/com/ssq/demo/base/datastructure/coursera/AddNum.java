fba4f6ec6d4365cc63c47ab94c71387b76f74d7f
sushiqi <sushiqi@meishubao.com>
2022-02-01 19:41:08 +0800

    coursera - 两数之和

---
+++ src/main/java/com/ssq/demo/base/datastructure/coursera/AddNum.java
@@ -0,0 +1,16 @@
+package com.ssq.demo.base.datastructure.coursera;
+
+import java.util.Scanner;
+
+public class AddNum {
+    static int addNum(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(addNum(a, b));
+    }
+}
