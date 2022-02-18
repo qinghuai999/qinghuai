27db4472541644cc095b5136419870c520390c3b
sushiqi <sushiqi@meishubao.com>
2022-02-18 17:08:31 +0800

    斐波那契,取余

--- src/main/java/com/ssq/demo/base/datastructure/coursera/FibonacciOtherNum.java
+++ src/main/java/com/ssq/demo/base/datastructure/coursera/FibonacciOtherNum.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class FibonacciOtherNum {
 
-    private static BigDecimal getFibonacciHugeNaive(long n, long m) {
+    private static BigDecimal getFibonacciHuge1(long n, long m) {
         if (n <= 0) {
             return BigDecimal.valueOf(n);
         }
@@ -30,10 +30,43 @@ public class FibonacciOtherNum {
         return bigDecimals[bigDecimals.length - 1];
     }
 
+    // 查询斐波那契数列和的最后一位数[优化]
+    static BigDecimal[][] initMatirx = {{BigDecimal.valueOf(1), BigDecimal.valueOf(1)}, {BigDecimal.valueOf(1), BigDecimal.valueOf(0)}};
+    static BigDecimal[][] unitMatirx = {{BigDecimal.valueOf(1), BigDecimal.valueOf(0)}, {BigDecimal.valueOf(0), BigDecimal.valueOf(1)}};
+    private static BigDecimal getFibonacciHuge(long n, long k) {
+        if(n <= 0) {
+            return BigDecimal.valueOf(0);
+        }
+        if (n == 1 || n == 2) {
+            return BigDecimal.valueOf(1);
+        }
+        BigDecimal[][] result = unitMatirx;
+        BigDecimal[][] tem = initMatirx;
+        long m = n - 2;
+        while (m != 0){
+            if ((m & 1) == 1) {
+                result = matirxMulti(tem, result);
+            }
+            tem = matirxMulti(tem, tem);
+            m >>= 1;
+        }
+        BigDecimal[] bigDecimals = result[0][0].add(result[1][0]).divideAndRemainder(BigDecimal.valueOf(k));
+        return bigDecimals[bigDecimals.length-1];
+    }
+
+    private static BigDecimal[][] matirxMulti(BigDecimal[][] a, BigDecimal[][] b) {
+        BigDecimal[][] temp = new BigDecimal[2][2];
+        temp[0][0] = a[0][0].multiply(b[0][0]).add(a[0][1].multiply(b[1][0]));
+        temp[0][1] = a[0][0].multiply(b[0][1]).add(a[0][1].multiply(b[1][1]));
+        temp[1][0] = a[1][0].multiply(b[0][0]).add(a[1][1].multiply(b[1][0]));
+        temp[1][1] = a[1][0].multiply(b[0][1]).add(a[1][1].multiply(b[1][1]));
+        return temp;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+        System.out.println(getFibonacciHuge(n, m));
     }
 }
