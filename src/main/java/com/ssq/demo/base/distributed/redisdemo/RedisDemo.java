92eb616358c8313b8030ace14a989084cd2cdb9b
sushiqi <16644101@qq.com>
2023-02-24 17:24:32 +0800

    check package name

---
+++ src/main/java/com/ssq/demo/base/distributed/redisdemo/RedisDemo.java
@@ -0,0 +1,15 @@
+package com.ssq.demo.base.distributed.redisdemo;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+
+import java.util.concurrent.locks.Lock;
+
+public class RedisDemo {
+
+    public static void main(String[] args) {
+//        RLock lock = redisson.getLock("123");
+//        Lock
+    }
+    
+}
---
+++ src/main/java/com/ssq/demo/redis/redisdemo/RedisOperation.java
@@ -0,0 +1,10 @@
+package com.ssq.demo.redis.redisdemo;
+
+/**
+ * @Author: 苏诗淇
+ * @CreateDate: 2021-01-30 16:28:01
+ * @Description: Redis操作 - 数据类型
+ */
+public class RedisOperation {
+
+}
